ab7e4d5d2c9c4aadfd065afd6161a87d61dcdec8
Michele Bigi <mikbigi@gmail.com>
2026-01-20 16:35:40 +0100

    Initial Commit

---
+++ FdC Railway Manager/FdC_Railway_ManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FdC_Railway_ManagerApp.swift
+//  FdC Railway Manager
+//
+//  Created by Michele Bigi on 20/01/26.
+//
+
+import SwiftUI
+
+@main
+struct FdC_Railway_ManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
